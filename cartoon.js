0db3375622c09f5e7b53d5bf1195fa1de8279962
xww55 <140449893+xww55@users.noreply.github.com>
2026-05-17 04:36:08 -0400

    Update cartoon.js

--- cartoon.js
+++ cartoon.js
@@ -6,10 +6,8 @@ ctx.canvas.width = 900;
 ctx.canvas.height = 500;
 ctx.canvas.style.border = "5px solid black";
 
-
-
 // sky
-ctx.fillStyle = "#91d2fa";
+ctx.fillStyle = "#b5e9ff";
 ctx.fillRect(0, 0, 900, 500);
 
 // sun
@@ -20,18 +18,56 @@ ctx.fill();
 
 // ground
 ctx.fillStyle = "#79e55e";
-ctx.fillRect(0, 430, 900, 200);
+ctx.fillRect(0, 370, 900, 200);
 
 // text in canvas
 ctx.fillStyle = "black";
-ctx.font = "35px Arial";
+ctx.font = "20px Arial";
 ctx.textAlign = "center";
-ctx.fillText("A bright day with a peaceful house.", 400, 90);
+ctx.fillText("A bright day with a peaceful house.", 450, 50);
 
-// house
-ctx.fillStyle = "#ebad87";
-ctx.fillRect(350, 200, 180, 140);
+// house - rectangle
+ctx.fillStyle = "#ffe0b5";
+ctx.fillRect(350, 230, 270, 190);
+// roof
+ctx.fillStyle = "#624125";
+ctx.beginPath();
+ctx.moveTo(350, 230);   // left eave
+ctx.lineTo(485, 100);   // peak
+ctx.lineTo(620, 230);   // right eave
+ctx.closePath();
+ctx.fill();
 
-// ctx.fillStyle = "#624125";
+// windows
+ctx.fillStyle = "#97c4c9";
+ctx.fillRect(400, 260, 50, 50);
+ctx.fillRect(400, 340, 50, 50);
+ctx.fillRect(520, 260, 50, 50);
+ctx.fillRect(520, 340, 50, 50);
+// door
+ctx.fillStyle = "#624125";
+ctx.fillRect(462, 340, 45, 80);
 
+// door knob
+ctx.fillStyle = "#ffcc00";
+ctx.beginPath();
+ctx.arc(498, 387, 5, 0, 2 * Math.PI);
+ctx.fill();
 
+// grass
+let bunch = 100;
+ctx.strokeStyle = "#396912";
+ctx.lineWidth = 2;
+ 
+for (var i = 0; i < 5; i++) {
+    ctx.save();
+    
+    ctx.translate(bunch, 440);
+ 
+    ctx.beginPath(); ctx.moveTo(0, 0); ctx.lineTo(-7, -25); ctx.stroke(); // left blade
+    ctx.beginPath(); ctx.moveTo(0, 0); ctx.lineTo(0, -33);  ctx.stroke(); // center blade
+    ctx.beginPath(); ctx.moveTo(0, 0); ctx.lineTo(7, -25);  ctx.stroke(); // right blade
+ 
+    bunch += 170;
+    ctx.restore();
+}
