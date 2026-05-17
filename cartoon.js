e412df9320bc56b7120419fe41bcc5136edf3c8c
xww55 <140449893+xww55@users.noreply.github.com>
2026-05-17 02:40:48 -0400

    Update cartoon.js

--- cartoon.js
+++ cartoon.js
@@ -2,25 +2,25 @@ document.body.style.backgroundColor = "#d88aa2";
 document.body.style.textAlign = "center";
 
 const ctx = document.getElementById("cartoonCanv").getContext("2d");
-ctx.width = 900;
-ctx.height = 500;
-ctx.style.border = "5px solid black";
+ctx.canvas.width = 900;
+ctx.canvas.height = 500;
+ctx.canvas.style.border = "5px solid black";
 
 
 
 // sky
 ctx.fillStyle = "#91d2fa";
-ctx.fillRect(0, 0, 800, 500);
+ctx.fillRect(0, 0, 900, 500);
 
 // sun
 ctx.fillStyle = "yellow";
 ctx.beginPath();
-ctx.arc(90, 70, 0, 2*Math.PPI);
+ctx.arc(90, 70, 40, 0, 2*Math.PI);
 ctx.fill();
 
 // ground
 ctx.fillStyle = "#79e55e";
-ctx.fillRect(0, 430, 800, 200);
+ctx.fillRect(0, 430, 900, 200);
 
 // text in canvas
 ctx.fillStyle = "black";
@@ -33,4 +33,5 @@ ctx.fillStyle = "#ebad87";
 ctx.fillRect(350, 200, 180, 140);
 
 // ctx.fillStyle = "#624125";
+
 
